a5c6c4ff1abdbf15cdf825ff8f356bfd92419f87
Isaac Batista <isaacbatst@gmail.com>
2022-11-12 23:11:10 -0300

    :white_check_mark: add pawn tests

--- src/domain/entities/Pieces/Pawn/Pawn.test.ts
+++ src/domain/entities/Pieces/Pawn/Pawn.test.ts
@@ -64,6 +64,36 @@ describe('Pawn', () => {
 				expect(avaliableMoves).toContainEqual(new Position(Row.FIVE, Col.A));
 			});
 		});
+
+		describe('Given next row is occupied', () => {
+			it('should not allow move foward', () => {
+				const board = new Board();
+				const pawn2b = new Pawn('any-id-1', new Position(Row.TWO, Col.B), PieceColor.WHITE);
+				const pawn7b = new Pawn('any-id-2', new Position(Row.THREE, Col.B), PieceColor.BLACK);
+				board.addPiece(pawn2b);
+				board.addPiece(pawn7b);
+
+				const avaliableMoves = pawn2b.getAvailableMoves(board);
+
+				expect(avaliableMoves).toHaveLength(0);
+			});
+		});
+
+		describe('Given a pawn at H column', () => {
+			it('should allow move foward and left diagonal', () => {
+				const board = new Board();
+				const pawn2g = new Pawn('any-id-1', new Position(Row.TWO, Col.G), PieceColor.WHITE);
+				const pawn3h = new Pawn('any-id-2', new Position(Row.THREE, Col.H), PieceColor.BLACK, true);
+				board.addPiece(pawn2g);
+				board.addPiece(pawn3h);
+
+				const avaliableMoves = pawn3h.getAvailableMoves(board);
+
+				expect(avaliableMoves).toHaveLength(2);
+				expect(avaliableMoves).toContainEqual(new Position(Row.TWO, Col.G));
+				expect(avaliableMoves).toContainEqual(new Position(Row.TWO, Col.H));
+			});
+		});
 	});
 
 	describe('Blacks', () => {
@@ -124,6 +154,20 @@ describe('Pawn', () => {
 				expect(avaliableMoves).toContainEqual(new Position(Row.FOUR, Col.B));
 				expect(avaliableMoves).toContainEqual(new Position(Row.FOUR, Col.C));
 				expect(avaliableMoves).toContainEqual(new Position(Row.FOUR, Col.A));
+			});
+		});
+
+		describe('Given next row is occupied', () => {
+			it('should not allow move foward', () => {
+				const board = new Board();
+				const pawn2b = new Pawn('any-id-1', new Position(Row.TWO, Col.B), PieceColor.WHITE);
+				const pawn7b = new Pawn('any-id-2', new Position(Row.THREE, Col.B), PieceColor.BLACK);
+				board.addPiece(pawn2b);
+				board.addPiece(pawn7b);
+
+				const avaliableMoves = pawn7b.getAvailableMoves(board);
+
+				expect(avaliableMoves).toHaveLength(0);
 			});
 		});
 	});
